b7f868a2339aa592c18cc7a6da76099ed5ff8c14
alfenfebral <alfenfebral@gmail.com>
2018-06-17 19:34:39 +0700

    First Commit

---
+++ api/models/Articles.js
@@ -0,0 +1,20 @@
+/**
+ * Articles.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    title: {
+      type: 'string'
+    },
+    body: {
+      type: 'string'
+    }
+  },
+  datastore: 'mongodb'
+};
+
